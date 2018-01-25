499323363506450626943f2ec6ded50819cb0013
n.horner <n.horner@har.mrc.ac.uk>
2018-01-25 15:51:29 +0000

    updated version

--- _version.py
+++ _version.py
@@ -1,3 +1,4 @@
-__version__ = '1.8.1' # Ported to Qt5 to take advantage of high res monitor scaling
+__version__ = '1.8.2' # Slice synchroisation Bug fixes
+#__version__ = '1.8.1' # Ported to Qt5 to take advantage of high res monitor scaling
 #__version__ = '1.8.0' # Rleased juut before Christmas 2017
 
