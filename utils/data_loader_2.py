10de6b52e5031538d5c754afeb0f1e6c3b5b2f7c
Neil Horner <n.horner@har.mrc.ac.uk>
2019-08-27 13:40:47 +0100

    Added dataloader2 for generic loading of data into vpy

---
+++ utils/data_loader_2.py
@@ -0,0 +1,86 @@
+"""
+Load volumes into vpv from a tml config file
+
+Examples
+--------
+
+Example toml file
+
+orientation = 'sagittal'
+
+
+[top]
+specimens = [
+'path1.nrrd',
+'path1.nrrd',
+'path2.nrrd',
+'path3.nrrd']
+
+[bottom]
+specimens = [
+'path1.nrrd',
+'path1.nrrd',
+'path2.nrrd',
+'path3.nrrd']
+
+"""
+import sys
+from pathlib import Path
+from itertools import chain
+
+import toml
+from PyQt5 import QtGui
+
+from vpv.vpv import Vpv
+from vpv.common import Layers
+
+test_file = sys.argv[1]
+
+config = toml.load(test_file)
+
+top_vols = config['top']['specimens']
+
+
+bottom = config['bottom']['specimens']
+if bottom:
+    bottom_vols =  config['bottom']['specimens']
+else: # We allow only top vier visible
+    bottom_specs = []
+    bottom_vols = []
+    bottom_labels = []
+
+app = QtGui.QApplication([])
+ex = Vpv()
+
+p2s = lambda x: [str(z) for z in x]
+
+all_vols = top_vols + bottom_vols
+ex.load_volumes(chain(p2s(top_vols), p2s(bottom_vols)), 'vol')
+
+
+# Set the top row of views
+for i in range(3):
+    try:
+        vol_id = Path(top_vols[i]).stem
+        ex.views[i].layers[Layers.vol1].set_volume(vol_id)
+    except IndexError:
+        continue
+
+if bottom:
+    # Set the top row of views
+    for i in range(3):
+        try:
+            vol_id = Path(bottom_vols[i]).stem
+            ex.views[i + 3].layers[Layers.vol1].set_volume(vol_id)
+        except IndexError:
+            continue
+
+print('Finished loading')
+
+# Show two rows
+ex.data_manager.show2Rows(True if bottom else False)
+
+# Set orientation
+ex.data_manager.on_orientation(config['orientation'])
+
+sys.exit(app.exec_())
