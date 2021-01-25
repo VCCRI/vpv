22efeebd8bad3ed9a757f6dcbb048daf90c1afa4
Neil Horner <horner.neil@gmail.com>
2021-01-25 19:55:24 +0000

    tewak loder

--- vpv/loading_scripts/loader.py
+++ vpv/loading_scripts/loader.py
@@ -4,7 +4,7 @@ Given a directory of images, this script will load them based on a config.
 See example 'loader.yaml'
 """
 
-
+import copy
 from pathlib import Path
 import yaml
 from PyQt5 import QtGui
@@ -20,6 +20,7 @@ def resolve_wildcard_paths(paths, root):
         res.append(p)
     return res
 
+
 def load(config_path, root_dir):
 
     with open(config_path, 'r') as fh:
@@ -29,8 +30,9 @@ def load(config_path, root_dir):
 
     # Build the views from the templates
     views = []
+
     for v in config['views']:
-        view = template.copy()
+        view = copy.deepcopy(template)
         if v.get('top'):
             for k, p in v['top'].items():
                 view['top'][k] = p
@@ -43,12 +45,19 @@ def load(config_path, root_dir):
     ex = Vpv()
 
     vpv_ids = []
+
+    already_loaded = {}
     for view in views:
         top_vol = Path(view['top']['path'])
         bottom_vol = Path(view['bottom']['path'])
         vols = resolve_wildcard_paths([top_vol, bottom_vol], root_dir)
 
-        vpv_ids.extend(ex.load_volumes(vols, 'vol'))
+        for v in vols:
+            if v in already_loaded:
+                vpv_ids.append(already_loaded[v])
+            else:
+                vpv_ids.extend(ex.load_volumes([v], 'vol'))
+                already_loaded[v] = vpv_ids[-1]
 
     for i, view in enumerate(views):
 
@@ -62,26 +71,14 @@ def load(config_path, root_dir):
 
         ex.views[i].set_orientation(Orientation[view['ori']])
 
-        ex.views[i].layers[Layers.vol2].set_opacity(view['top']['opacity'])
+        if view['bottom'].get('opacity'):
+            ex.views[i].layers[Layers.vol2].set_opacity(view['bottom']['opacity'])
 
         ex.views[i].layers[Layers.vol1].set_lut(view['top']['color'])
         ex.views[i].layers[Layers.vol2].set_lut(view['bottom']['color'])
 
-
     # Show two rows
     ex.data_manager.show2Rows(True if len(views) > 3 else False)
-
-    # Set orientation
-    # ex.data_manager.on_orientation('sagittal')
-
-    # Set colormap
-    # ex.data_manager.on_vol2_lut_changed('anatomy_labels')
-
-    # opacity
-    # ex.data_manager.modify_layer(Layers.vol2, 'set_opacity', config.opacity)
-
-    # Set title
-    # ex.mainwindow.setWindowTitle(config['title'])
 
     sys.exit(app.exec_())
 
